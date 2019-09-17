bdd89c1af1d5a4f24bffa0ab11aed16edf51bf72
Zixin Luo <zxluo.cn@gmail.com>
2019-09-17 10:43:13 +0800

    init gl3d_v2

--- utils/io.py
+++ utils/io.py
@@ -6,8 +6,44 @@ IO tools.
 
 from __future__ import print_function
 
+import os
+import re
 from struct import unpack
 import numpy as np
+
+
+def load_pfm(pfm_path):
+    with open(pfm_path, 'rb') as fin:
+        color = None
+        width = None
+        height = None
+        scale = None
+        data_type = None
+        header = str(fin.readline().decode('UTF-8')).rstrip()
+
+        if header == 'PF':
+            color = True
+        elif header == 'Pf':
+            color = False
+        else:
+            raise Exception('Not a PFM file.')
+
+        dim_match = re.match(r'^(\d+)\s(\d+)\s$', fin.readline().decode('UTF-8'))
+        if dim_match:
+            width, height = map(int, dim_match.groups())
+        else:
+            raise Exception('Malformed PFM header.')
+        scale = float((fin.readline().decode('UTF-8')).rstrip())
+        if scale < 0:  # little-endian
+            data_type = '<f'
+        else:
+            data_type = '>f'  # big-endian
+        data_string = fin.read()
+        data = np.fromstring(data_string, data_type)
+        shape = (height, width, 3) if color else (height, width)
+        data = np.reshape(data, shape)
+        data = np.flip(data, 0)
+    return data
 
 
 def read_corr(file_path):
@@ -23,7 +59,7 @@ def read_corr(file_path):
     with open(file_path, 'rb') as fin:
         while True:
             rin = fin.read(24)
-            if rin == '':
+            if len(rin) == 0:
                 # EOF
                 break
             idx0, idx1, num = unpack('L' * 3, rin)
@@ -38,10 +74,10 @@ def read_kpt(file_path):
     Args:
         file path: file path.
     Returns:
-        kpt_data: keypoint data of Nx9 numpy array.
+        kpt_data: keypoint data of Nx6 numpy array.
     """
     kpt_data = np.fromfile(file_path, dtype=np.float32)
-    kpt_data = np.reshape(kpt_data, (-1, 9))
+    kpt_data = np.reshape(kpt_data, (-1, 6))
     return kpt_data
 
 
@@ -76,3 +112,37 @@ def read_mask(file_path, size=14):
         mask = np.array(decoded[1:])
         mask_dict[decoded[0]] = mask
     return mask_dict
+
+
+def read_cams(cam_path):
+    """
+    Args:
+        cam_path: Path to cameras.txt.
+    Returns:
+        cam_dict: A dictionary indexed by image index and composed of (K, t, R, dist, img_size).
+        K - 2x3, t - 3x1, R - 3x3, dist - 1x3, img_size - 1x2.
+    """
+    cam_data = [i.split(' ') for i in read_list(cam_path)]
+
+    cam_dict = {}
+    for i in cam_data:
+        i = [float(j) for j in i if j is not '']
+        K = np.array([(i[1], i[5], i[3]),
+                      (0, i[2], i[4]), (0, 0, 1)])
+        t = np.array([(i[6], ), (i[7], ), (i[8], )])
+        R = np.array([(i[9], i[10], i[11]),
+                      (i[12], i[13], i[14]),
+                      (i[15], i[16], i[17])])
+        dist = np.array([i[18], i[19], i[20]])
+        img_size = np.array([i[21], i[22]])
+        cam_dict[i[0]]= (K, t, R, dist, img_size)
+    return cam_dict
+
+
+def read_list(list_path):
+    """Read list."""
+    if list_path is None or not os.path.exists(list_path):
+        print('Not exist', list_path)
+        exit(-1)
+    content= open(list_path).read().splitlines()
+    return content
